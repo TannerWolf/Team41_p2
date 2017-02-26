4d3b5d879e47326eb2c319c8f915845036142122
Tanner <tanwolf67@gmail.com>
2017-02-26 15:57:26 -0600

    First commit

---
+++ src/JobListIterator.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class JobListIterator<E> implements Iterator<E> {
+
+	Listnode<E> head;
+	
+	public JobListIterator(Listnode<E> header) {
+		head = header;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E next() {
+		// TODO Auto-generated method stub
+
+		//if() {
+			//throw new NoSuchElementException();
+		//}
+		return null;
+	}
+
+	
+}
