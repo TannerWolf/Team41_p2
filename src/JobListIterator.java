defd37aeb093bf22f22c5477220b70e13da0d284
Crilliam <willmustari@gmail.com>
2017-03-02 15:46:04 -0600

    Update JobListIterator.java

--- src/JobListIterator.java
+++ src/JobListIterator.java
@@ -1,3 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2017
+// PROJECT:          team41_p2
+// FILE:             JobListIterator
+//
+// TEAM:    Team 41, IDGAF
+// Authors: 
+// Author1: (Jarrett Benson, jbenson6@wisc.edu, jbenson6, Lec 002)
+// Author2: (Cameron Carlson, ccarlson24@wisc.edu, ccarlson, Lec 002) 
+// Author3: (Isaac Heinrich, iheinrich@wisc.edu, iheinrich, Lec 002) 
+// Author4: (Jiayue Lai, jlai28@wisc.edu, jlai, Lec 002)
+// Author5: (William Mustari, willmustari@gmail.com, mustari, Lec 002) 
+// Author6: (Tanner Wolf, tmwolf2@wisc.edu, tmwolf2, Lec 002) 
+///////////////////////////////////////////////////////////////////////////////
+
 import java.util.*;
 
 public class JobListIterator<E> implements Iterator<Job> {
