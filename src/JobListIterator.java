309211e5710e728c8ba22f6e5d18a98a5eb21ef5
Crilliam <willmustari@gmail.com>
2017-03-05 18:16:32 -0600

    Update JobListIterator.java

--- src/JobListIterator.java
+++ src/JobListIterator.java
@@ -15,22 +15,30 @@
 
 import java.util.*;
 
+/**
+ * The JobListIterator class contains methods to check if there is a next node
+ * and get the next nodes data if it exists.
+ */
 public class JobListIterator<E> implements Iterator<Job> {
 	
 	Listnode<Job> curr;		//node reference we will traverse with
-
+	
 	public JobListIterator(Listnode<Job> head) {
 		curr = head.getNext();	//start curr at first node in the list
 	}
 
 	@Override
-			//checks if there is a next node
+	/**
+	 * Checks if there is a next node
+	 */
 	public boolean hasNext() {	
 		return curr != null;	//returns false if the next reference is null
 	}
 
 	@Override
-			//get the next node's data if it exists
+	/**
+	 * Get the next node's data if it exists
+	 */
 	public Job next() throws NoSuchElementException {
 		
 			//throw an error if there isn't a next node
